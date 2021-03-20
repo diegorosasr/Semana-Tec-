fc0d05ab518e4abb72736186d77f7a9b764e30d2
diegorosasr <80845586+diegorosasr@users.noreply.github.com>
2021-03-20 08:35:38 -0600

    Update conv_simple.py

--- conv_simple.py
+++ conv_simple.py
@@ -11,16 +11,18 @@ from skimage import io
 import matplotlib.pyplot as plt
 
 def convolve2d(image, kernel):
-    kernel = np.flipud(np.fliplr(kernel))   
+    kernel = np.flipud(np.fliplr(kernel))    
     output = np.zeros_like(image)            
     
+    # Add zero padding to the input image 
     image_padded = np.zeros((image.shape[0] + (kernel.shape[0]-1), 
                              image.shape[1] + (kernel.shape[1]-1)))   
     image_padded[(kernel.shape[0]//2):-(kernel.shape[0]//2), 
                  (kernel.shape[1]//2):-(kernel.shape[1]//2)] = image
     
-    for x in range(image.shape[1]):   
+    for x in range(image.shape[1]):     
         for y in range(image.shape[0]):
+            # element-wise multiplication of the kernel and the image
             output[y,x]=(kernel*image_padded[y:y + kernel.shape[0], x:x + kernel.shape[1]]).sum()
     return output
 
@@ -38,7 +40,6 @@ kernel = kernel/k_elements
 cnvlvd_img = convolve2d(img,kernel)
 print('\n First 5 columns and rows of the standar average matrix: \n', cnvlvd_img[:5,:5]*255)
 
-# Plot the filtered image
 plt.imshow(cnvlvd_img, cmap=plt.cm.gray)
 plt.axis('off')
 plt.show()
